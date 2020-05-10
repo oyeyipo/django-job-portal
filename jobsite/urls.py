f6b4e2c56675c47751b734d028c991376f38a54b
Olawale Oyeyipo <olaclassik@gmail.com>
2020-05-10 22:52:41 +0100

    wiring homepage views

--- jobsite/urls.py
+++ jobsite/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="test.html")),
+    path("", include("jobs.urls")),
+    path("test/", TemplateView.as_view(template_name="test.html")),
     path("admin/", admin.site.urls),
 ]
 
