96abac1110cb11ea5bf3ff96d79259d3ecc9e5d0
Olawale Oyeyipo <olaclassik@gmail.com>
2020-05-10 22:51:28 +0100

    test chore

--- e2e/base.py
+++ e2e/base.py
@@ -7,9 +7,32 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 
 
+MAX_WAIT = 10
+
+
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit()
+        super().tearDown()
+
+    @wait
+    def wait_for(self, fn):
+        return fn()
--- e2e/test_create_job_listings.py
+++ e2e/test_create_job_listings.py
@@ -1,15 +1,28 @@
+from unittest import skip
+
+from selenium.webdriver.common.keys import Keys
 
 from .base import FunctionalTest
 
 
 class JobListingsTest(FunctionalTest):
-
     def test_landing_page(self):
+        # Prospective Employee (PE) visits bbracesjobs website for
+        # job opportunity
         self.browser.get(self.live_server_url)
 
+        # They notices the page mentions Jobs and Careers
         self.assertIn("Jobs and Careers", self.browser.title)
+
+        # Also there is a big text mentioning the company and site purpose
         header_text = self.browser.find_element_by_tag_name("h1").text
         self.assertIn("Business Brace Jobs and Careers", header_text)
 
-    def test_can_create_job_item(self):
-        pass
+        # a call-action-button was visible, asking to show opened
+        # positions
+        # Clicked on the CTA button as they see list of vacancy
+        # Also noticing they are still on the same page.
+        self.fail("Finish the test!")
+
+    # def test_can_create_job_item(self):
+    #     pass
