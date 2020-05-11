2dda64460d73fa3ea9a952d70ddc9d22b7cbfd7f
Olawale Oyeyipo <olaclassik@gmail.com>
2020-05-11 18:19:19 +0100

    connect users model to admin

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
+    AbstractUser,
 )
 from django.db import models
 
@@ -19,23 +20,20 @@ class UserManager(BaseUserManager):
     to create `User` objects.
     """
 
-    def create_user(self, email, username, password=None):
+    def create_user(self, email, password=None):
         """
         Creates and saves a User with the given email, username and password.
         """
         if not email:
             raise ValueError("Users must have an email address")
 
-        if not username:
-            raise ValueError("Users must have a username.")
-
-        user = self.model(email=self.normalize_email(email), username=username)
+        user = self.model(email=self.normalize_email(email))
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password):
+    def create_superuser(self, email, password):
         """
         Create and return a `User` with superuser powers.
 
@@ -45,7 +43,7 @@ class UserManager(BaseUserManager):
         if password is None:
             raise TypeError("Superusers must have a password")
 
-        user = self.create_user(email, password=password, username=username)
+        user = self.create_user(email, password=password)
 
         user.is_superuser = True
         user.is_admin = True
@@ -55,7 +53,9 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin, TimestampedModel):
-    username = models.CharField(db_index=True, max_length=255, unique=True)
+    # username = models.CharField(db_index=True, max_length=255, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
@@ -63,7 +63,7 @@ class User(AbstractBaseUser, PermissionsMixin, TimestampedModel):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         """
@@ -75,11 +75,10 @@ class User(AbstractBaseUser, PermissionsMixin, TimestampedModel):
 
     def get_full_name(self):
         """
-        This method is required by Django for things like handling emails.
-        Typically, this would be the user's first and last name. Since we do
-        not store the user's real name, we return their username instead.
+        Return the first_name plus the last_name, with a space in between.
         """
-        return self.username
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
 
     def get_short_name(self):
         """
@@ -87,7 +86,7 @@ class User(AbstractBaseUser, PermissionsMixin, TimestampedModel):
         Typically, this would be the user's first name. Since we do not store
         the user's real name, we return their username instead.
         """
-        return self.username
+        return self.first_name
 
     @property
     def is_staff(self):
